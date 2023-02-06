58cd1d6ed88d80d5dd91e368dc01898e026f7412
Feyza Doğan <41289364+afeyzadogan@users.noreply.github.com>
2023-02-06 13:14:15 +0300

    Add files via upload
    7. ödev

---
+++ sql_hw7.sql
@@ -0,0 +1,17 @@
+-- 1-
+SELECT rating FROM film  GROUP BY rating
+
+-- 2-
+SELECT replacement_cost, COUNT(title) FROM film  
+GROUP BY replacement_cost
+HAVING COUNT(*)>50
+
+-- 3-
+SELECT store_id, COUNT(customer_id) FROM customer
+GROUP BY store_id 
+
+-- 4-
+SELECT country_id, COUNT(city) FROM city
+GROUP BY country_id
+ORDER BY COUNT(city) DESC
+LIMIT 1
