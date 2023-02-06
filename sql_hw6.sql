c3a42b42a032b240d0e032691521e88aced762e9
Feyza Doğan <41289364+afeyzadogan@users.noreply.github.com>
2023-02-06 11:09:48 +0300

    Add files via upload
    6. ödev

---
+++ sql_hw6.sql
@@ -0,0 +1,16 @@
+-- 1-
+SELECT AVG(rental_rate) FROM film;
+
+-- 2- 
+SELECT COUNT(title)
+FROM film
+WHERE title LIKE 'C%';
+
+-- 3-
+SELECT MAX(length)
+FROM film
+WHERE rental_rate = 0.99
+
+-- 4-
+SELECT DISTINCT COUNT(replacement_cost) FROM film
+WHERE rental_duration > 150;
